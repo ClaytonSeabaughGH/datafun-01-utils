2025453181b05d6abe6efb3e531f0bb6ff65a2e9
Clayton Seabaugh <claytonseabaugh@gmail.com>
2024-08-27 16:33:27 -0500

    Iteration 2

--- utils_seabaugh.py
+++ utils_seabaugh.py
@@ -1,35 +1,41 @@
 
-''' ITERATION 1
+'''  ITERATION 2
 
-Module: Intersteller Analytics - Reusable Module for My Data Analytics Projects
+Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
 
 This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
 
 Process:
 
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
+In this second iteration, add a function that returns the byline as a string.
 
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
+I'll create a function named get_byline().
+It'll return my byline to whatever calls the get_byline() function. 
    
-2. I'll declare a global variable for my byline string and just set it to some simple text.
+I'll update the main() function to use the new get_byline() function. 
 
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
+Same conditional boilerplate at the end. 
 
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
+I'll test this version before adding more code that shows:
+- my ability to declare variables of different types
+- my ability to use Python to calculate basic descriptive statistics. '''
 
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
-'''
 
 #####################################
 # Declare a global variable named byline.
 #####################################
 
 byline: str = 'Intersteller Analytics: Delivering Professional Insights'
+
+
+
+#####################################
+# Define the get_byline() Function
+#####################################
+
+def get_byline()-> str:
+    '''Return byline for my analytics projects'''
+    return byline
 
 #####################################
 # Define a main() function for this module.
@@ -46,7 +52,7 @@ byline: str = 'Intersteller Analytics: Delivering Professional Insights'
 
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
-    print(byline)
+    print(get_byline())
 
 #####################################
 # Conditional Execution - Only call main() when executing this module as a script.
