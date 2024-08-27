7b18524dce9e472b475882e3cd8b0b3f6bcd0891
Clayton Seabaugh <claytonseabaugh@gmail.com>
2024-08-27 18:12:26 -0500

    Iteration 3

--- utils_seabaugh.py
+++ utils_seabaugh.py
@@ -1,5 +1,4 @@
-
-'''  ITERATION 2
+'''  ITERATION 3
 
 Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
 
@@ -7,55 +6,75 @@ This module provides a simple, reusable foundation for my analytics projects.
 
 Process:
 
-In this second iteration, add a function that returns the byline as a string.
+In this third iteration, I declare additional variables to show skills with different data types.
 
-I'll create a function named get_byline().
-It'll return my byline to whatever calls the get_byline() function. 
-   
-I'll update the main() function to use the new get_byline() function. 
-
-Same conditional boilerplate at the end. 
-
-I'll test this version before adding more code that shows:
-- my ability to declare variables of different types
-- my ability to use Python to calculate basic descriptive statistics. '''
-
+'''
 
 #####################################
-# Declare a global variable named byline.
+# Declare global variables - keep byline at the end
+# We will use this information in a smarter byline
 #####################################
 
-byline: str = 'Intersteller Analytics: Delivering Professional Insights'
+# Boolean variable to indicate if the company has international clients
+has_international_clients: bool = True
 
+# Integer variable for the number of years in operation
+years_in_operation: int = 10
 
+# List of strings representing the skills offered by the company
+skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+
+# List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+
+# Boolean variable to indicate if the company is looking for new clients
+accepting_new_clients: bool = True
+
+# Integer variable for the number of employees in the company
+number_of_employees: int = 4
+
+# List of strings reprseting different locations of clients
+locations_of_clients: list = ["United States", "United Kingdom","France","Germany" ]
+
+# List of floats representing amount of clinets per location
+clients_per_location: list = [51, 32, 24, 13]
+#####################################
+# Declare a global variable named byline. 
+# Make it a multiline f-string to show our information.
+#####################################
+
+byline: str = f"""
+---------------------------------------------------------
+Interstellar Analytics: Delivering Professional Insights
+---------------------------------------------------------
+Has International Clients:  {has_international_clients}
+Years in Operation:         {years_in_operation}
+Skills Offered:             {skills_offered}
+Client Satisfaction Scores: {client_satisfaction_scores}
+Accepting New Clients:      {accepting_new_clients}
+Number of Employees:        {number_of_employees}
+Locations of Clients:       {locations_of_clients}
+Clients per Location:       {clients_per_location}
+"""
 
 #####################################
 # Define the get_byline() Function
 #####################################
 
-def get_byline()-> str:
-    '''Return byline for my analytics projects'''
+def get_byline() -> str:
+    '''Return a byline for my analytics projects.'''
     return byline
 
 #####################################
 # Define a main() function for this module.
 #####################################
 
-# Create a function named main.
-# A function is a block of code that performs a specific task.
-# This function will simply print the byline to the console.
-# Add a type hint to indicate that this function doesn't return anything when called 
-# (that is, it has a Python type of None).
-# It doesn't need any additional information passed in, 
-# so there's nothing needed inside the parentheses.
-# Everything afer the colon (:) must be indented (usually 4 spaces)
-
 def main() -> None:
-    '''Print the byline to the console when this function is called.'''
+    '''Print results of get_byline() when main() is called.'''
     print(get_byline())
 
 #####################################
-# Conditional Execution - Only call main() when executing this module as a script.
+# Conditional Execution
 #####################################
 
 if __name__ == '__main__':
